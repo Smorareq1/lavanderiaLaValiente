beeecc7aa456ca639d5c193aa8fda7bf5fa64fb5
Smorareq1 <smorareq1@gmail.com>
2024-07-08 21:46:59 -0600

    Correo

--- js/script.js
+++ js/script.js
@@ -41,4 +41,39 @@
 
 })(jQuery);
 
+//Forms
+(function(){
+    emailjs.init("1WlQQETpRAMa9pEC-");
+})();
 
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('informacionAdicionalForm');
+    const btnEnviar = document.getElementById('btninformacionAdicional');
+    const nombreInput = document.getElementById('NombreinformacionAdicional');
+    const correoInput = document.getElementById('CorreoinformacionAdicional');
+    const descripcionInput = document.getElementById('DescripcioninformacionAdicional');
+
+    btnEnviar.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const nombre = nombreInput.value;
+        const correo = correoInput.value;
+        const descripcion = descripcionInput.value;
+
+        emailjs.send("service_28k3fjp", "template_7cuh8ih", {
+            nombre: nombre,
+            correo: correo,
+            descripcion: descripcion
+        })
+            .then((response) => {
+                alert('Correo enviado!');
+                nombreInput.value = '';
+                correoInput.value = '';
+                descripcionInput.value = '';
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                alert('Hubo un problema al enviar el correo. Por favor, intenta nuevamente.');
+            });
+    });
+});
